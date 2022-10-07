dc24e20381717de5780477da707bf5ae87945b25
Ivan Heng Ze Hao <ivanhengzehao@gmail.com>
2022-10-08 03:29:51 +0800

    master - Add todo item testing + update README.md

--- test/java/com/vuevmc/stepdefs/MyStepdefs.java
+++ test/java/com/vuevmc/stepdefs/MyStepdefs.java
@@ -3,44 +3,76 @@ package com.vuevmc.stepdefs;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class MyStepdefs {
     public WebDriver driver;
+    public WebElement inputField;
+    public WebElement todoItem;
+    public String todoItemToInput;
+
 
     @Before
     public void setup() {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
-        driver.get("https://todomvc.com/examples/vue/");
     }
 
     @After
-    public void quit() {
+    public void teardown() {
         driver.quit();
+    }
+
+    @Given("I am on the todo list page")
+    public void iAmOnTheTodoListPage() {
+        driver.get("https://todomvc.com/examples/vue/");
     }
 
     @And("I see an input field")
     public void iSeeAnInputField() {
+        inputField = driver.findElement(By.cssSelector(".new-todo"));
+
+        if(!inputField.isDisplayed()) {
+            throw new AssertionError("Input field is found but not displayed");
+        }
     }
 
     @When("I select the input field")
     public void iSelectTheInputField() {
+        inputField.click();
     }
 
-    @And("I enter <todo>")
-    public void iEnterTodo() {
+    @And("I enter a {} item in the input field")
+    public void iEnterTodoItem(String todo) {
+        todoItemToInput = todo;
+        inputField.sendKeys(todoItemToInput);
     }
 
     @And("I press Enter")
     public void iPressEnter() {
+        inputField.sendKeys(Keys.ENTER);
     }
 
-    @Then("I should see new thing added into the todo list")
-    public void iShouldSeeNewThingAddedIntoTheTodoList() {
+    @Then("I should see new todo item added")
+    public void iShouldSeeNewThingAddedAsTodo() {
+        todoItem = driver.findElement(By.cssSelector(".todo label"));
+        if (!todoItem.getText().equals(todoItemToInput)) {
+            throw new AssertionError("Added todo item is incorrect");
+        }
+    }
+
+    @And("I should see input field being empty")
+    public void iShouldSeeInputFieldBeingEmpty() {
+        if(!inputField.getAttribute("value").equals("")) {
+            throw new AssertionError("Input field is not empty.");
+        }
     }
 }
---
+++ test/resources/features/ManageTodo.feature
@@ -0,0 +1,36 @@
+Feature: Manage todo
+  I can manage my todo list
+
+  Scenario Outline: Add different kinds of todo items
+    Given I am on the todo list page
+    And I see an input field
+    When I select the input field
+    And I enter a <todo> item in the input field
+    And I press Enter
+    Then I should see new todo item added
+    And I should see input field being empty
+
+    Examples:
+      | todo |
+      | fruit |
+      | Fruit |
+      | Fruits!! |
+      | 1 |
+      | 1 fruit |
+      | 2 IPhones @ Orchard |
+
+#    Scenario: Delete single todo item
+#      Given I am on the todo list page
+#      And I have added 1 todo item
+#      When I hover over the added todo item
+#      And I see 'X' icon appear
+#      And I select the 'X' icon
+#      Then I see the todo item deleted
+#
+#  Scenario: Bulk delete multiple todo items
+#    Given I am on the todo list page
+#    And I have added 2 todo items
+#    When I select button at left of input field
+#    And I see 'Clear completed' appear
+#    And I select 'Clear completed'
+#    Then I see the todo item deleted
